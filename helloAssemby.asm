2d417d217e026eaf24fc9f1c18a068e7df7751d5
Malcolm Treacy <mtreacy@dhcp-v039-247.mobile.uci.edu>
2019-04-17 15:23:55 -0700

    Added documentation to first program

--- helloAssemby.asm
+++ helloAssemby.asm
@@ -1,4 +1,12 @@
+# Author: Malcolm Treacy
+# Date: 4.17.19
+# First assembly program: Prints "Hello World!" into the I/O console.
+
+# Assembly Information (for learning purposes)
+# .data: The area where the data being used in my program will be stored.
+# .text: The area where the assembly instructions will be defined.
 .data 
+   # Declares a RAM variable 'myMessage' containing "Hello World\n"
    myMessage: .asciiz "Hello World\n"
 .text
    # Notifying the system to get ready to print something
