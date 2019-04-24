8f67c49913dad3c1b77fb188ec4a97aaf455eee0
Malcolm Treacy <mtreacy@dhcp-v060-112.mobile.uci.edu>
2019-04-24 10:42:37 -0700

    First assembly program

--- 1_hello_world.asm
+++ 1_hello_world.asm
@@ -8,10 +8,17 @@
 .data 
    # Declares a RAM variable 'myMessage' containing "Hello World\n"
    myMessage: .asciiz "Hello World\n"
+   myName: .ascii "My name is Malcolm, this is my first assembly program.\n"
 .text
    # Notifying the system to get ready to print something
    li $v0, 4
    # Load the address from RAM to $a0
    la $a0, myMessage
    # syscall makes the program execute the the procedure
+   syscall
+   # Notify the system I want to print something:
+   li $v0, 4
+   # Load my second sring into the argument register:
+   la $a0, myName
+   # Notify the assembler to print my value to the console:
    syscall
